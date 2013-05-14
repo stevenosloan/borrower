153418940e6c8ed13563a0b543e2df5ec18affb3
Steven Sloan <stevenosloan@gmail.com>
2013-05-14 00:49:17 -0400

    add false test for Borrower::Path.remote?

--- spec/path_spec.rb
+++ spec/path_spec.rb
@@ -8,5 +8,9 @@ describe Borrower::Path do
       Borrower::Path.remote?("http://google.com").should be_true
     end
 
+    it "recognizes a local path" do
+      Borrower::Path.remote?("/path/to/file.rb").should be_false
+    end
+
   end
 end
